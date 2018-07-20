a1824d1dff945b883af7918d1acfc2b6d8508572
Ruben Van Coile <Ruben.VanCoile@UGent.be>
2018-07-19 21:47:57 -0400

    change local name visualization.py => visualizationME.py (issue with identical filename in rvcpy)

--- PhaseSix.py
+++ PhaseSix.py
@@ -13,7 +13,7 @@ import numpy as np
 import pandas as pd
 
 import matplotlib.pyplot as plt
-from visualization import LinePlot_XmultiY
+from visualizationME import LinePlot_XmultiY
 
 from copy import deepcopy
 
